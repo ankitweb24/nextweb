00f6267f29f83b79a265d2a9af258611911c43c6
Ankit Jangir <61105805+ankitweb24@users.noreply.github.com>
2023-06-08 19:20:30 +0530

    Update next.config.js
    using serverAction

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+   experimental : {
+        appDir : true,
+        serverComponentsExternalPackages : ['mongoose'],
+        serverActions : true
+    }
+}
 
 module.exports = nextConfig
