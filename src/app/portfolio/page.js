c1c0f6cb7393efb4cc199dc0ca0c309218f31ffb
Ankit Jangir <61105805+ankitweb24@users.noreply.github.com>
2023-06-13 22:17:53 +0530

    Update page.js

--- src/app/portfolio/page.js
+++ src/app/portfolio/page.js
@@ -1,11 +1,9 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import Projects from "../components/Projects";
 const page = () => {
   return (
    <>
     <Header/>
-    <Projects/>
     <Footer/>
    </>
   )
